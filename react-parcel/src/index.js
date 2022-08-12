784ad2d172c93f743653840508dfc953b2826b6b
tjaard123 <tjaard123@gmail.com>
2022-08-12 20:28:14 +0200

    Add basic bootstrap and icons

--- react-parcel/src/index.js
+++ react-parcel/src/index.js
@@ -1,5 +1,40 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import { UserCircleAlt } from "iconoir-react";
 
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
+import * as bootstrap from "bootstrap";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid d-flex justify-content-between">
+        <a class="navbar-brand" href="#">
+          TITLE
+        </a>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <UserCircleAlt color="white" />
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </main>
+  </div>
+);
